9c5a71188b325ba1359a6468c6abfa7a988d6323
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-01 18:21:32 +0800

    Create BackgroundController.ts

--- assets/resources/fly/typeScript/BackgroundController.ts
+++ assets/resources/fly/typeScript/BackgroundController.ts
@@ -1,19 +1,13 @@
-import { _decorator, Component, Node, Vec3 } from 'cc';
+import { _decorator, Component, Node, UITransform} from 'cc';
 const { ccclass, property } = _decorator;
 
-@ccclass('BackgroundController')
-export class BackgroundController extends Component {
+@ccclass('CanvasController')
+export class CanvasController extends Component {
     start() {
-
     }
 
     update(deltaTime: number) {
-        for (let children of this.node.children) {
-            children.setPosition(new Vec3(children.position.x, children.position.y - 50 * deltaTime, children.position.z));
-            if (children.position.y <= -240) {
-                children.setPosition(new Vec3(children.position.x, 0, children.position.z));
-            }
-        }
+        
     }
 }
 
