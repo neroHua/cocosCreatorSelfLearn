c5ccd56bac873b4991467bec0ee2ba0e3f5a2096
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-01 18:22:11 +0800

    Update CanvasController.ts

--- assets/resources/fly/typeScript/CanvasController.ts
+++ assets/resources/fly/typeScript/CanvasController.ts
@@ -4,8 +4,6 @@ const { ccclass, property } = _decorator;
 @ccclass('CanvasController')
 export class CanvasController extends Component {
     start() {
-        const uiTransform = this.getComponent(UITransform);
-        uiTransform.setContentSize(320, 480);
     }
 
     update(deltaTime: number) {
