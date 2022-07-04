37f75c9238fd40cd8eb374ebd061dd3a4f641c4b
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-04 15:29:31 +0800

    Update BulletController.ts

--- assets/resources/fly/typeScript/BulletController.ts
+++ assets/resources/fly/typeScript/BulletController.ts
@@ -1,14 +1,22 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('BulletController')
 export class BulletController extends Component {
+
+  @property
+  speed: number = 960;
+
     start() {
 
     }
 
     update(deltaTime: number) {
-        
+        this.node.setPosition(new Vec3(this.node.position.x, this.node.position.y + 480 * deltaTime, 0));
+
+        if (this.node.position.y > 480) {
+          this.node.destroy();
+        }
     }
 }
 
