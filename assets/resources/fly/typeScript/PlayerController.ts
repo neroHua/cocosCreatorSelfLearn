0d039cfd3f77927debd145970b35fa1cd4ce5232
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-04 15:17:43 +0800

    Update PlayerController.ts

--- assets/resources/fly/typeScript/PlayerController.ts
+++ assets/resources/fly/typeScript/PlayerController.ts
@@ -1,13 +1,23 @@
-import { _decorator, Component, Node, NodeEventType, Vec2, Vec3 } from 'cc';
+import { _decorator, Component, Node, NodeEventType, Vec2, Vec3, Prefab, instantiate, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
+
+    @property(Prefab)
+    bullet: Prefab = null;
+
     start() {
       this.node.on(NodeEventType.TOUCH_MOVE, (event) => {
         const touchLocation = event.getLocation();
         this.node.setPosition(new Vec3(touchLocation.x - 320, touchLocation.y - 480, 0));
       });
+
+      this.schedule(() => {
+        let bullet = instantiate(this.bullet);
+        bullet.setParent(this.node.parent);
+        bullet.setPosition(new Vec3(this.node.position.x, this.node.position.y + 56, 0));
+      }, 0.5);
     }
 
     update(deltaTime: number) {
