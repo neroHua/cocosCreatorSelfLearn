a0c54f1482a00251c61f559cefbe30578df169e3
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-06 10:41:25 +0800

    Update EnemyController.ts

--- assets/resources/fly/typeScript/EnemyController.ts
+++ assets/resources/fly/typeScript/EnemyController.ts
@@ -3,13 +3,27 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class EnemyController extends cc.Component {
 
+    @property
+    dead: boolean = false;
+
     start () {
 
     }
 
-    // update (dt) {}
+    update (dt) {
+      if (this.dead) {
+        return;
+      }
+      if (this.node.y <= 0) {
+        this.node.destroy();
+      }
+
+      this.node.y -= 300 * dt;
+    }
 
     die() {
+      this.dead = true;
+
       cc.loader.loadRes("fly/image/enemy_die", cc.SpriteFrame, (error, res) => {
         this.node.getComponent(cc.Sprite).spriteFrame = res;
       })
