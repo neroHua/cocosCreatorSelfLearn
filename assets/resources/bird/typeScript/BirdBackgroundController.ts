981477671a8c1f8f47ba5e457f5649233aeb4a88
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-07 17:27:33 +0800

    Update BirdBackgroundController.ts

--- assets/resources/bird/typeScript/BirdBackgroundController.ts
+++ assets/resources/bird/typeScript/BirdBackgroundController.ts
@@ -2,6 +2,13 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class BirdBackgroundController extends cc.Component {
+
+    @property
+    speed: number = 36;
+
+    @property
+    width: number = 288;
+
     start () {
     }
 
@@ -10,15 +17,15 @@ export default class BirdBackgroundController extends cc.Component {
       const background1 = backgroundList[0]; 
       const background2 = backgroundList[1]; 
 
-      background1.x -= 36 * dt;
-      background2.x -= 36 * dt;
+      background1.x -= this.speed * dt;
+      background2.x -= this.speed * dt;
 
-      if (background1.x <= -288) {
-        background1.x = background2.x + 288;
+      if (background1.x <= -this.width) {
+        background1.x = background2.x + this.width;
       }
 
-      if (background2.x <= -288) {
-        background2.x = background1.x + 288;
+      if (background2.x <= -this.width) {
+        background2.x = background1.x + this.width;
       }
     }
 }
