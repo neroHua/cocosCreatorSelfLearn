96ab1217479c84f675349b205f01b73fdbf98926
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-07 10:43:17 +0800

    Create BackgroundController.ts

---
+++ assets/resources/bird/typeScript/BackgroundController.ts
@@ -0,0 +1,24 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class BackgroundController extends cc.Component {
+    start () {
+    }
+
+    update (dt) {
+      const backgroundList = this.node.children;
+      const background1 = backgroundList[0]; 
+      const background2 = backgroundList[1]; 
+
+      background1.x -= 36 * dt;
+      background2.x -= 36 * dt;
+
+      if (background1.x <= -288) {
+        background1.x = background2.x + 288;
+      }
+
+      if (background2.x <= -288) {
+        background2.x = background1.x + 288;
+      }
+    }
+}
