cac73222b11ccaf15e94130c2659628c3048446c
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-07 14:51:23 +0800

    Update BirdController.ts

--- assets/resources/bird/typeScript/BirdController.ts
+++ assets/resources/bird/typeScript/BirdController.ts
@@ -3,20 +3,33 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class BirdController extends cc.Component {
 
+    @property
+    direction: cc.Vec2 = cc.v2(0, -1)
+
     onLoad () {
       cc.director.getPhysicsManager().enabled = true;
     }
 
     start () {
-      let rigidBody: cc.RigidBody = this.node.getComponent(cc.RigidBody);
+      // let rigidBody: cc.RigidBody = this.node.getComponent(cc.RigidBody);
 
       // rigidBody.applyForce(cc.v2(5000, 0), cc.v2(0, 0), true)
       // rigidBody.applyForceToCenter(cc.v2(5000, 0), true);
 
-      rigidBody.linearVelocity = cc.v2(20, 0);
+      // rigidBody.linearVelocity = cc.v2(20, 0);
     }
 
-    // update (dt) {}
+    update (dt) {
+      this.node.x += 40 * this.direction.x * dt;
+      this.node.y += 40 * this.direction.y * dt;
+
+      const res = cc.director.getPhysicsManager().rayCast(this.node.position, cc.v2(this.node.x + this.direction.x * 100, this.node.y + this.direction.y * 100), cc.RayCastType.Closest);
+      console.log(res, this.direction.y);
+
+      if (res.length >= 1) {
+        this.direction.y *= -1;
+      }
+    }
 
     onBeginContact(contact: cc.PhysicsContact, self, other) {
       const contactPointList = contact.getWorldManifold().points;
