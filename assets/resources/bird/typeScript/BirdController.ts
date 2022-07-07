cb1a0000c1a6a7d32636a780d583b7067dac264a
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-07 19:26:20 +0800

    Update BirdController.ts

--- assets/resources/bird/typeScript/BirdController.ts
+++ assets/resources/bird/typeScript/BirdController.ts
@@ -11,84 +11,37 @@ export default class BirdController extends cc.Component {
     }
 
     start () {
-      // let rigidBody: cc.RigidBody = this.node.getComponent(cc.RigidBody);
-
-      // rigidBody.applyForce(cc.v2(5000, 0), cc.v2(0, 0), true)
-      // rigidBody.applyForceToCenter(cc.v2(5000, 0), true);
-
-      // rigidBody.linearVelocity = cc.v2(20, 0);
-
-      // let action = cc.moveTo(2, 200, 200)
-      // let action = cc.moveBy(2, 200, 200)
-      // let action = cc.rotateTo(2, 100);
-      // let action = cc.scaleTo(2, 1.5)
-      // let action = cc.jumpTo(2, 200 , 100, 200, 3);
-      // let action = cc.blink(2, 3);
-      // let action = cc.blink(2, 3);
-      // let action = cc.fadeOut(2);
-      // let action = cc.fadeIn(2);
-      // let action = cc.tintTo(2, 100, 30, 100);
-      // let action = cc.delayTime(1);
-      // let action = cc.show();
-      // let action = cc.hide();
-      // let action = cc.toggleVisibility();
-      // let action = cc.flipX(true);
-      // let action = cc.flipY(true);
-      // let action = cc.callFunc(() => {
-
-      // });
-
-      // let action1 = cc.fadeOut(2);
-      // let action2 = cc.fadeIn(2);
-      // let sequence = cc.sequence(action1, action2);
-      // this.node.runAction(sequence);
-      // let repeat = cc.repeat(sequence, 3);
-      // let repeatForever = cc.repeatForever(sequence);
-      // this.node.runAction(repeat);
-
-      // let action1 = cc.tintTo(2, 100, 30, 100);
-      // let action2 = cc.moveTo(2, 200, 200)
-      // let spawn = cc.spawn(action1, action2);
-      // this.node.runAction(spawn);
-
-      // action.setTag(333);
-      // this.node.stopAction(action);
-      // this.node.pauseAllActions();
-      // this.node.resumeAllActions();
-
-      let animation = this.node.getComponent(cc.Animation)
-      animation.play();
-
+      // let animation = this.node.getComponent(cc.Animation)
+      // animation.play();
     }
 
     update (dt) {
-      // this.node.x += 40 * this.direction.x * dt;
-      // this.node.y += 40 * this.direction.y * dt;
-
-      // const res = cc.director.getPhysicsManager().rayCast(this.node.position, cc.v2(this.node.x + this.direction.x * 100, this.node.y + this.direction.y * 100), cc.RayCastType.Closest);
-      // console.log(res, this.direction.y);
-
-      // if (res.length >= 1) {
-      //   this.direction.y *= -1;
-      // }
     }
 
     fly() {
       let rigidBody = this.node.getComponent(cc.RigidBody);
-      rigidBody.linearVelocity = cc.v2(0, 300);
+      rigidBody.linearVelocity = cc.v2(0, 150);
     }
 
+
     onBeginContact(contact: cc.PhysicsContact, self, other) {
-      const contactPointList = contact.getWorldManifold().points;
-      const normal = contact.getWorldManifold().normal;
-      console.log('开始碰撞', contactPointList, normal);
+      console.log('开始碰撞');
+      if (other.tag == 11) {
+        console.log('碰撞地面');
+      }
+      if (other.tag == 12) {
+        console.log('碰撞上管道');
+      }
+      if (other.tag == 13) {
+        console.log('碰撞下管道');
+      }
+      if (other.tag == 14) {
+        console.log('通过管道标识');
+      }
     }
 
     onEndContact(contact: cc.PhysicsContact, self, other) {
       console.log('碰撞结束');
     }
     
-    customerEvent() {
-      console.log("自定义动画中的事件");
-    }
 }
